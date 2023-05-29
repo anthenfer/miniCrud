c6699361bf3ebd035620eb981cd062f84ef6cb29
Antonio Henrique <antoniohenriqueferreira@outlook.com.br>
2023-05-29 12:53:26 -0300

    Finalizando CREATE de CRUD

--- main.js
+++ main.js
@@ -6,6 +6,27 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+const tempClient = {
+    nome: "Rafael",
+    email: "rafael@email.com",
+    celular: "19985649874",
+    cidade: "Hortolândia"
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? [];
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient));
+
+/// CRUD - Create Read Update Delete
+
+
+/// CRUD - CREATE
+const createClient = (client) => {
+    const dbClient = getLocalStorage();
+    dbClient.push(client);    
+    setLocalStorage(dbClient);
+}
+
+/// Eventos
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
